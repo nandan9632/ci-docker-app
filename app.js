191fa1ca1983b151c37873eda9ee0aa3d803822e
nandan9632 <nandantn25@gmail.com>
2026-02-17 14:01:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('CI Pipeline Success 🚀');
+  res.send("Frontend route working ");
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    status: "success",
+    message: "Backend API working bro "
+  });
 });
 
 app.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log("Server running on port 3000");
 });
